fe0dad14fb80076b3f8c44b6f5699f19f6d8f637
ArchX1-erik <elegenstein@student.tgm.ac.at>
2016-02-12 12:11:48 +0100

    Write impl added

--- src/SocketControl.java
+++ src/SocketControl.java
@@ -1,5 +1,51 @@
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  * Created by fusions on 12.02.16.
  */
 public class SocketControl {
+    private String host;
+    private int port;
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getHost() {
+
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    /**
+     *
+     * @param addr ip addr of the service endpoint
+     * @param port port of the service endpoint
+     */
+    public SocketControl(String addr, int port){
+
+    }
+
+    public void writeMsg(String msg){
+        Socket socket = null;
+        try {
+            socket = new Socket(host, port);
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
+            pw.print(msg);
+            pw.close();
+        } catch (java.io.IOException e) {
+            System.err.print(e);
+        } finally {
+            socket.close();
+        }
+    }
 }
